6535413b3e9175648d7fd0af17e6a2f570ed682b
amarjitmahadik <amarjit.mahadik@maxlifeinsurance.com>
2023-11-25 08:59:37 +0530

    Revert "added a new function1"
    This reverts commit a79b95ea80e8df0738c1c25c05109590812ddfd4.

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,11 @@
 print('addingall the data processing code related to fraud detection')
+print('adding one message')
+
+a=5
+b=6
+
+def sum(a,b):
+        c=a+b
+        return a
+
+print('addeda new function sum and it is incorrrect')
